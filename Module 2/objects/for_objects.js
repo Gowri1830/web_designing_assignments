e1fac3cd9cf3eb889f9fa142b7a09537495ff4ed
Gowri1830 <134362118+Gowri1830@users.noreply.github.com>
2023-07-24 08:29:47 +0530

    24th july

---
+++ Module 2/objects/for_objects.js
@@ -0,0 +1,9 @@
+const student={
+    fname: "sahail",
+    lname: "dutta",
+    age: 19,
+    country: "India"
+};
+for (i in student){
+    console.log(student[i]);
+}
---
+++ Module 2/objects/nested_objects.js
@@ -0,0 +1,11 @@
+const person =
+{
+    name: "Ruchi",
+    age: 19,
+    address: {
+        street: "123 main road",
+        city: "Delhi",
+        Pincode: "560016"
+    }
+};
+console.log(person.address.Pincode);
